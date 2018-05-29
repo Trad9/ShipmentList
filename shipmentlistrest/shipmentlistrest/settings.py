da9ab67ff21c238c533df0c29f715b68b4006d91
Trad9 <Trad9@users.noreply.github.com>
2018-05-29 10:52:09 +0300

    remove auth

--- shipmentlistrest/shipmentlistrest/settings.py
+++ shipmentlistrest/shipmentlistrest/settings.py
@@ -127,15 +127,3 @@ if DEBUG:
     DEFAULT_RENDERER_CLASSES = DEFAULT_RENDERER_CLASSES + (
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-
-    ),
-    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
-}
